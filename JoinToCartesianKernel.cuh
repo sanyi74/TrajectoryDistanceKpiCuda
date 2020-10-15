c456d74535e5864d4834bba0f3ecdfa6b28b68f5
SandorRacz <sandor.racz.74@gmail.com>
2020-10-15 15:20:17 +0200

    Update calculation.

--- JoinToCartesianKernel.cuh
+++ JoinToCartesianKernel.cuh
@@ -58,6 +58,34 @@ __device__ __host__ REAL Sqrt(REAL x) {
 #endif
 }
 
+void inline forward(const float* q, REAL* T, float d1, float a2, float a3, float d4, float d5, float d6) {
+	// Source: https://github.com/ros-industrial/universal_robot/blob/kinetic-devel/ur_kinematics/src/ur_kin.cpp
+	REAL s1, c1; SinCos(*q, &s1, &c1); q++;
+	REAL q23 = *q, q234 = *q; REAL s2, c2;  SinCos(*q, &s2, &c2); q++;
+	REAL s3, c3; SinCos(*q, &s3, &c3); q23 += *q; q234 += *q; q++;
+	REAL s4, c4;  SinCos(*q, &s4, &c4);	q234 += *q; q++;
+	REAL s5, c5;  SinCos(*q, &s5, &c5);	q++;
+	REAL s6, c6;  SinCos(*q, &s6, &c6);
+	REAL s23, c23;  SinCos(q23, &s23, &c23);
+	REAL s234, c234;  SinCos(q234, &s234, &c234);
+
+	REAL A = (s1 * s5 + c234 * c1 * c5);
+	REAL B = (c1 * s5 - c234 * c5 * s1);
+
+	*T = c234 * c1 * s5 - c5 * s1; T++;
+	*T = c6 * A - s234 * c1 * s6; T++;
+	*T = -s6 * A - s234 * c1 * c6; T++;
+	*T = d6 * c234 * c1 * s5 - a3 * c23 * c1 - a2 * c1 * c2 - d6 * c5 * s1 - d5 * s234 * c1 - d4 * s1; T++;
+	*T = c1 * c5 + c234 * s1 * s5; T++;
+	*T = -c6 * B - s234 * s1 * s6; T++;
+	*T = s6 * B - s234 * c6 * s1; T++;
+	*T = d6 * (c1 * c5 + c234 * s1 * s5) + d4 * c1 - a3 * c23 * s1 - a2 * c2 * s1 - d5 * s234 * s1; T++;
+	*T = -s234 * s5; T++;
+	*T = -c234 * s6 - s234 * c5 * c6; T++;
+	*T = s234 * c5 * s6 - c234 * c6; T++;
+	*T = d1 + a3 * s23 + a2 * s2 - d5 * (c23 * c4 - s23 * s4) - d6 * s5 * (c23 * s4 + s23 * c4);
+}
+
 __device__ __host__ void jointToCartesian(unsigned int index, DhParameter* dhParameters, Pose* poses, JointPoint* jointPoints, CartesianPoint* cartesianPoints) {
 
 	unsigned int dhIndex = jointPoints[index].dhIndex;
@@ -67,108 +95,72 @@ __device__ __host__ void jointToCartesian(unsigned int index, DhParameter* dhPar
 	REAL d4 = dhParameters[dhIndex].d4;
 	REAL d5 = dhParameters[dhIndex].d5;
 	REAL d6 = dhParameters[dhIndex].d6;
-	REAL SIN_Z_ROTATION, COS_Z_ROTATION;
-	SinCos(dhParameters[dhIndex].rotZ, &SIN_Z_ROTATION, &COS_Z_ROTATION);
-
-	REAL j1, j2, s0, s1, s2, s4, s5, s123, c0, c1, c2, c4, c5, c123;
-	j1 = jointPoints[index].q[1];
-	j2 = jointPoints[index].q[2];
-	SinCos(jointPoints[index].q[0], &s0, &c0);
-	SinCos(j1, &s1, &c1);
-	SinCos(j2, &s2, &c2);
-	SinCos(jointPoints[index].q[4], &s4, &c4);
-	SinCos(jointPoints[index].q[5], &s5, &c5);
-	SinCos(j1 + j2 + jointPoints[index].q[3], &s123, &c123);
-
-	REAL A1 = c0 * c123 + s0 * s123;
-	REAL A2 = c0 * c123 - s0 * s123;
-	REAL A3 = s0 * c123 + c0 * s123;
-	REAL A4 = s0 * c123 - c0 * s123;
-	REAL A5 = c123 * c4 + s123 * s4;
-	REAL A6 = c123 * c4 - s123 * s4;
-
+	
 	REAL T[12];
-	T[0] = (A2 * s4) / (REAL)(2.0) - c4 * s0 + (A1 * s4) / (REAL)(2.0);
-	T[1] = (c5 * (s0 * s4 + (A2 * c4) / (REAL)(2.0) + (A1 * c4) / (REAL)(2.0)) - (s5 * (A3 - A4)) / (REAL)(2.0));
-	T[2] = (-(c5 * (A3 - A4)) / (REAL)(2.0) - s5 * (s0 * s4 + (A2 * c4) / (REAL)(2.0) + (A1 * c4) / (REAL)(2.0)));
-	T[3] = ((d5 * A4) / (REAL)(2.0) - (d5 * A3) / (REAL)(2.0) - d4 * s0 + (d6 * A2 * s4) / (REAL)(2.0) + (d6 * A1 * s4) / (REAL)(2.0) - a2 * c0 * c1 - d6 * c4 * s0 - a3 * c0 * c1 * c2 + a3 * c0 * s1 * s2);
-	T[4] = c0 * c4 + (A3 * s4) / (REAL)(2.0) + (A4 * s4) / (REAL)(2.0);
-	T[5] = (c5 * ((A3 * c4) / (REAL)(2.0) - c0 * s4 + (A4 * c4) / (REAL)(2.0)) + s5 * (A2 / (REAL)(2.0) - A1 / (REAL)(2.0)));
-	T[6] = (c5 * (A2 / (REAL)(2.0) - A1 / (REAL)(2.0)) - s5 * ((A3 * c4) / (REAL)(2.0) - c0 * s4 + (A4 * c4) / (REAL)(2.0)));
-	T[7] = ((d5 * A2) / (REAL)(2.0) - (d5 * A1) / (REAL)(2.0) + d4 * c0 + (d6 * A3 * s4) / (REAL)(2.0) + (d6 * A4 * s4) / (REAL)(2.0) + d6 * c0 * c4 - a2 * c1 * s0 - a3 * c1 * c2 * s0 + a3 * s0 * s1 * s2);
-	T[8] = (A6 / (REAL)(2.0) - A5 / (REAL)(2.0));
-	T[9] = ((s123 * c5 - c123 * s5) / (REAL)(2.0) - (s123 * c5 + c123 * s5) / (REAL)(2.0) - s123 * c4 * c5);
-	T[10] = (s123 * c4 * s5 - (c123 * c5 + s123 * s5) / (REAL)(2.0) - (c123 * c5 - s123 * s5) / (REAL)(2.0));
-	T[11] = (d1 + (d6 * A6) / (REAL)(2.0) + a3 * (s1 * c2 + c1 * s2) + a2 * s1 - (d6 * A5) / (REAL)(2.0) - d5 * c123);
+	forward(jointPoints[index].q, T, d1, a2, a3, d4, d5, d6);
 
-	REAL Ttemp[8];
-	Ttemp[0] = T[0] * COS_Z_ROTATION - T[4] * SIN_Z_ROTATION;
-	Ttemp[1] = T[1] * COS_Z_ROTATION - T[5] * SIN_Z_ROTATION;
-	Ttemp[2] = T[2] * COS_Z_ROTATION - T[6] * SIN_Z_ROTATION;
-	Ttemp[3] = T[3] * COS_Z_ROTATION - T[7] * SIN_Z_ROTATION;
-	Ttemp[4] = T[0] * SIN_Z_ROTATION + T[4] * COS_Z_ROTATION;
-	Ttemp[5] = T[1] * SIN_Z_ROTATION + T[5] * COS_Z_ROTATION;
-	Ttemp[6] = T[2] * SIN_Z_ROTATION + T[6] * COS_Z_ROTATION;
-	Ttemp[7] = T[3] * SIN_Z_ROTATION + T[7] * COS_Z_ROTATION;
-	for (unsigned int i = 0; i < 8; i++) T[i] = Ttemp[i];
+	REAL TZ[8], sinRotZ, cosRotZ;
+	SinCos(dhParameters[dhIndex].rotZ, &sinRotZ, &cosRotZ);
+	TZ[0] = T[0] * cosRotZ - T[4] * sinRotZ;
+	TZ[1] = T[1] * cosRotZ - T[5] * sinRotZ;
+	TZ[2] = T[2] * cosRotZ - T[6] * sinRotZ;
+	TZ[3] = T[3] * cosRotZ - T[7] * sinRotZ;
+	TZ[4] = T[0] * sinRotZ + T[4] * cosRotZ;
+	TZ[5] = T[1] * sinRotZ + T[5] * cosRotZ;
+	TZ[6] = T[2] * sinRotZ + T[6] * cosRotZ;
+	TZ[7] = T[3] * sinRotZ + T[7] * cosRotZ;
+	T[0] = TZ[0]; T[1] = TZ[1]; T[2] = TZ[2]; T[3] = TZ[3]; T[4] = TZ[4]; T[5] = TZ[5]; T[6] = TZ[6]; T[7] = TZ[7];
 
 	REAL quaternion[4], t;
 	if (T[0] + T[4 + 1] + T[2 * 4 + 2] > (REAL)(0.0)) {
 		t = T[0] + T[4 + 1] + T[2 * 4 + 2] + (REAL)(1.0);
 		quaternion[3] = t;
-		quaternion[2] = (T[1] - T[4]);
-		quaternion[1] = (T[2 * 4] - T[2]);
-		quaternion[0] = (T[4 + 2] - T[2 * 4 + 1]);
+		quaternion[2] = T[1] - T[4];
+		quaternion[1] = T[2 * 4] - T[2];
+		quaternion[0] = T[4 + 2] - T[2 * 4 + 1];
 	}
 	else if (T[0] > T[4 + 1] && T[0] > T[2 * 4 + 2]) {
 		t = T[0] - T[4 + 1] - T[2 * 4 + 2] + (REAL)(1.0);
 		quaternion[0] = t;
-		quaternion[1] = (T[1] + T[4]);
-		quaternion[2] = (T[2 * 4] + T[2]);
-		quaternion[3] = (T[4 + 2] - T[2 * 4 + 1]);
+		quaternion[1] = T[1] + T[4];
+		quaternion[2] = T[2 * 4] + T[2];
+		quaternion[3] = T[4 + 2] - T[2 * 4 + 1];
 	}
 	else if (T[4 + 1] > T[2 * 4 + 2]) {
 		t = -T[0] + T[4 + 1] - T[2 * 4 + 2] + (REAL)(1.0);
 		quaternion[1] = t;
-		quaternion[0] = (T[1] + T[4]);
-		quaternion[3] = (T[2 * 4] - T[2]);
-		quaternion[2] = (T[4 + 2] + T[2 * 4 + 1]);
+		quaternion[0] = T[1] + T[4];
+		quaternion[3] = T[2 * 4] - T[2];
+		quaternion[2] = T[4 + 2] + T[2 * 4 + 1];
 	}
 	else {
 		t = -T[0] - T[4 + 1] + T[2 * 4 + 2] + (REAL)(1.0);
 		quaternion[2] = t;
-		quaternion[3] = (T[1] - T[4]);
-		quaternion[0] = (T[2 * 4] + T[2]);
-		quaternion[1] = (T[4 + 2] + T[2 * 4 + 1]);
+		quaternion[3] = T[1] - T[4];
+		quaternion[0] = T[2 * 4] + T[2];
+		quaternion[1] = T[4 + 2] + T[2 * 4 + 1];
 	}
-	REAL s = reciprocalSqrt(t) * (REAL)(0.5);
-	quaternion[0] *= s;	quaternion[1] *= s;	quaternion[2] *= s;	quaternion[3] *= s;
+	t = reciprocalSqrt(t) * (REAL)(0.5);
+	quaternion[0] *= t;	quaternion[1] *= t;	quaternion[2] *= t;	quaternion[3] *= t;
 
 	unsigned int poseIndex = jointPoints[index].poseIndex;
-	cartesianPoints[index].xyz[0] = (float)(T[3] + poses[poseIndex].xyz[0]);
-	cartesianPoints[index].xyz[1] = (float)(T[7] + poses[poseIndex].xyz[1]);
+	cartesianPoints[index].xyz[0] = (float)(T[3] + poses[poseIndex].xyz[0]); 
+	cartesianPoints[index].xyz[1] = (float)(T[7] + poses[poseIndex].xyz[1]); 
 	cartesianPoints[index].xyz[2] = (float)(T[11] + poses[poseIndex].xyz[2]);
 
-	T[0] = poses[poseIndex].quaternion[0];
-	T[1] = poses[poseIndex].quaternion[1];
-	T[2] = poses[poseIndex].quaternion[2];
-	T[3] = poses[poseIndex].quaternion[3];
-
+	T[0] = poses[poseIndex].quaternion[0]; T[1] = poses[poseIndex].quaternion[1]; T[2] = poses[poseIndex].quaternion[2]; T[3] = poses[poseIndex].quaternion[3];
 	T[4] = quaternion[0] * T[3] + quaternion[1] * T[2] - quaternion[2] * T[1] + quaternion[3] * T[0];
 	T[5] = -quaternion[0] * T[2] + quaternion[1] * T[3] + quaternion[2] * T[0] + quaternion[3] * T[1];
 	T[6] = quaternion[0] * T[1] - quaternion[1] * T[0] + quaternion[2] * T[3] + quaternion[3] * T[2];
 	T[7] = -quaternion[0] * T[0] - quaternion[1] * T[1] - quaternion[2] * T[2] + quaternion[3] * T[3];
 
 	T[8] = Sqrt(T[4] * T[4] + T[5] * T[5] + T[6] * T[6] + T[7] * T[7]);
-	if (T[8] < (REAL)(0.0)) T[8] *= (REAL)(-1.0);
-	T[4] /= T[8];
-	T[5] /= T[8];
-	T[6] /= T[8];
-	T[7] /= T[8];
+	if (T[4] < (REAL)(0.0)) T[8] *= (REAL)(-1.0);
+	T[4] /= T[8]; T[5] /= T[8];	T[6] /= T[8]; T[7] /= T[8];
 
-	cartesianPoints[index].quaternion[0] = (float)(T[4]);
-	cartesianPoints[index].quaternion[1] = (float)(T[5]);
-	cartesianPoints[index].quaternion[2] = (float)(T[6]);
+	cartesianPoints[index].quaternion[0] = (float)(T[4]); 
+	cartesianPoints[index].quaternion[1] = (float)(T[5]); 
+	cartesianPoints[index].quaternion[2] = (float)(T[6]); 
 	cartesianPoints[index].quaternion[3] = (float)(T[7]);
 }
 
@@ -209,10 +201,10 @@ void evaluateJointToCartesian(unsigned int nSample, unsigned int nPoints, unsign
 	double cpuTime = 0;
 	double gpuTime = 0;
 	for (unsigned int iter = 0; iter < nSample; iter++) {
-
 		std::cout << "*";
 		auto start = std::chrono::system_clock::now();
 		{ // CPU
+
 			const unsigned int nThreads = 16;
 			std::thread* t[nThreads];
 			for (int i = 0; i < nThreads; i++) {
@@ -233,7 +225,6 @@ void evaluateJointToCartesian(unsigned int nSample, unsigned int nPoints, unsign
 				delete t[i];
 			}
 		}
-		// std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - start).count();
 
 		start = std::chrono::system_clock::now();
 		{ // GPU
@@ -241,9 +232,7 @@ void evaluateJointToCartesian(unsigned int nSample, unsigned int nPoints, unsign
 			unsigned int nBlocks = nPoints / 128;
 			if (nPoints % 128 != 0) nBlocks++;
 			for (int i = 0; i < nKernel; i++) jointToCartesianKernel << <nBlocks, 128 >> > (nPoints, dev_DhParameters, dev_Poses, dev_JointPoints, dev_CartesianPoints);
-			cudaDeviceSynchronize();
 			cudaMemcpy(host_CartesianPoints, dev_CartesianPoints, nPoints * sizeof(CartesianPoint), cudaMemcpyDeviceToHost);
-			cudaDeviceSynchronize();
 		}
 		gpuTime += std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - start).count();
 
@@ -283,9 +272,9 @@ int main() {
 		return -1;
 	}
 
-	evaluateJointToCartesian(16, 32, 1);
+	// evaluateJointToCartesian(64, 32, 1);
 
-	unsigned int nSamples = 16;
+	unsigned int nSamples = 4;
 	for (double i = 64 * 1024 * 1024; i >= 1000; i /= 1.414213562373095) {
 		evaluateJointToCartesian(nSamples, i, 0);
 		for (unsigned int k = 1; k <= 256; k *= 2) {
